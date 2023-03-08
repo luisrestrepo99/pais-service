b84638d8a86c9b765f970c58f3a772aee46f7448
Alvaro Salazar <asalazar@uceva.edu.co>
2023-03-08 17:19:11 -0500

    Creacion inicial de la capa de servicio y de entidades

---
+++ src/main/java/co/edu/uceva/pais_service/model/entities/Pais.java
@@ -0,0 +1,15 @@
+package co.edu.uceva.pais_service.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pais {
+    private Long id;
+    private String nombre;
+}
---
+++ src/main/java/co/edu/uceva/pais_service/model/service/PaisServiceImpl.java
@@ -0,0 +1,32 @@
+package co.edu.uceva.pais_service.model.service;
+
+import co.edu.uceva.pais_service.model.entities.Pais;
+
+import java.util.List;
+
+public class PaisServiceImpl implements IPaisService{
+    @Override
+    public Pais save(Pais pais) {
+        return null;
+    }
+
+    @Override
+    public void delete(Pais pais) {
+
+    }
+
+    @Override
+    public Pais update(Pais pais) {
+        return null;
+    }
+
+    @Override
+    public List<Pais> findAll() {
+        return null;
+    }
+
+    @Override
+    public Pais findById(Long id) {
+        return null;
+    }
+}
