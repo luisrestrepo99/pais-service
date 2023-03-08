a78045a2ec8246a0f9a3f97783205f6efdcd5ef8
Alvaro Salazar <asalazar@uceva.edu.co>
2023-03-08 08:31:03 -0500

    Se agrego un ejemplo de PathVariable

--- src/main/java/co/edu/uceva/pais_service/controller/PaisRestController.java
+++ src/main/java/co/edu/uceva/pais_service/controller/PaisRestController.java
@@ -1,6 +1,7 @@
 package co.edu.uceva.pais_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaisRestController {
 
     @GetMapping("/hola")
-    public String holaMundo(){
+    public String holaMundo(@PathVariable("nombre") String nombre){
         return "Hola mundo!";
     }
 }
